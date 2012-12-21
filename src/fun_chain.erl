b3da94629d5deab83d5ee31643c872add8779938
Sasa Juric <sasa@sasas-imac.lan>
2012-12-22 00:57:15 +0100

    replaced id_trans with generic deep walk of a parse tree

--- src/fun_chain.erl
+++ src/fun_chain.erl
@@ -1,15 +1,26 @@
-% The fun_chain parse transform. See README.md for usage
+%% The fun_chain parse transform. See README.md for usage
 
 -module(fun_chain).
--export([parse_transform/2, chain_calls/1]).
+-export([parse_transform/2]).
 
-parse_transform(Forms, _Options) -> erlang_tree:walk(Forms).
+parse_transform(ParseTree, _Options) -> deep_walk(ParseTree).
+
+deep_walk(List) when is_list(List) ->
+  [deep_walk(Element) || Element <- List];
+
+deep_walk({call, _Line, {atom, _, fun_chain}, [Initial | Calls]}) ->
+  chain_calls(Calls, Initial);
+
+deep_walk(Tuple) when is_tuple(Tuple) ->
+  list_to_tuple([deep_walk(Element) || Element <- tuple_to_list(Tuple)]);
+
+deep_walk(Other) -> Other.
   
-chain_calls([Initial | Calls]) -> do_chain_calls(Calls, Initial).
+chain_calls([], LastResult) -> LastResult;
 
-do_chain_calls([], LastResult) -> LastResult;
-do_chain_calls([CurrentCall | RemainingCalls], LastResult) ->
-  do_chain_calls(RemainingCalls, add_last_argument(CurrentCall, LastResult)).
+chain_calls([CurrentCall | RemainingCalls], LastResult) ->
+  chain_calls(RemainingCalls, add_last_argument(CurrentCall, LastResult)).
 
 add_last_argument({call, Line, Fun, Args}, Argument) ->
   {call, Line, Fun, Args ++ [Argument]}.
+  
