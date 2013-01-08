62fad395920f9d256448ae2402c99cc4b5f114e3
sasa1977 <sasa.juric@gmail.com>
2013-01-08 17:09:09 +0100

    dynamic generation of tests + code coverage

---
+++ test/dynamic_generator.erl
@@ -0,0 +1,38 @@
+% This module dynamically creates, compiles and executes test code.
+% This is used when testing parse_transform, so that the transform can be executed in the runtime
+% during the test. By doing this, we can check code coverage of the transform. In addition,
+% we can check that transform correctly generates errors.
+
+-module(dynamic_generator).
+-export([exec_dynamic/3]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+exec_dynamic(ModuleName, Head, Code) -> 
+  compile(ModuleName, Head, Code, fun(CompilationResult) ->
+    ?assertEqual({ok, ModuleName}, CompilationResult),
+    code:purge(ModuleName),
+    code:load_file(ModuleName),
+    ModuleName:test()
+  end).
+
+compile(ModuleName, Head, Code, Fun) ->
+  file:write_file(add_ext(ModuleName, "erl"), module_code(ModuleName, Head, Code)),
+  try
+    Fun(compile:file(add_ext(ModuleName, "erl"), [return_errors]))
+  after
+    file:delete(add_ext(ModuleName, "erl")),
+    file:delete(add_ext(ModuleName, "beam"))
+  end.
+
+module_code(ModuleName, Head, Code) -> 
+  Lines = [
+    ["-module('", atom_to_list(ModuleName), "')."],
+    "-export([test/0]).",
+    Head,
+    
+    ["test() ->", Code, "."]
+  ],
+  [[Line, "\n"] || Line <- Lines].
+
+add_ext(ModuleName, Extension) -> io_lib:format("~p.~s", [ModuleName, Extension]).
--- test/fun_chain_tests.erl
+++ test/fun_chain_tests.erl
@@ -5,46 +5,60 @@
 
 chain_last_test_() -> 
   [
-    ?_assertEqual(dict:new(), fun_chain:last(dict:new())),
+    ?_assertEqual(dict:new(), compile_dynamic("fun_chain:last(dict:new())")),
       
     ?_assertEqual(
       dict:from_list([{b,2}, {c,3}]),
-      fun_chain:last(dict:new(),
-        dict:store(a,1),
-        dict:store(b,2),
-        dict:erase(a),
-        dict:store(c,3)
-      )
+      compile_dynamic("
+        fun_chain:last(dict:new(),
+          dict:store(a,1),
+          dict:store(b,2),
+          dict:erase(a),
+          dict:store(c,3)
+        )
+      ")
     ),
     
     ?_assertEqual(
       dict:new(), 
-      fun_chain:last(fun_chain:last(dict:new()))
+      compile_dynamic("fun_chain:last(fun_chain:last(dict:new()))")
     ),
     
     ?_assertEqual(
       dict:from_list([{a,1}]), 
-      fun_chain:last(dict:new(),
-        (fun(Dict) ->
-          dict:store(a,1,Dict)
-        end)()
-      )
+      compile_dynamic("
+        fun_chain:last(dict:new(),
+          (fun(Dict) ->
+            dict:store(a,1,Dict)
+          end)()
+        )
+      ")
     ),
     
     ?_assertEqual(
       dict:from_list([{a,1}]), 
-      fun_chain:last(dict:new(),
-        (fun(Dict) ->
-          fun_chain:last(Dict, dict:store(a,1))
-        end)()
-      )
+      compile_dynamic("
+        fun_chain:last(dict:new(),
+          (fun(Dict) ->
+            fun_chain:last(Dict, dict:store(a,1))
+          end)()
+        )
+      ")
     )
   ].
   
   
 chain_first_test_() -> 
   [
-    ?_assertEqual(9, fun_chain:first(1, inc(3), inc(5)))
+    ?_assertEqual(9, compile_dynamic("
+      Inc = fun(X, Increment) -> X + Increment end,
+      fun_chain:first(1, Inc(3), Inc(5))
+    "))
   ].
-  
-inc(X, Inc) -> X + Inc.
+
+% Dynamic execution of the code. See dynamic_generator for explanation.
+compile_dynamic(Code) ->
+  dynamic_generator:exec_dynamic(dynamic_test,
+    "-compile({parse_transform, fun_chain}).",
+    Code
+  ).
