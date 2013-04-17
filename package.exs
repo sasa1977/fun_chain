e6eede57b74e7588f7aadcf21602023a21c18910
sasa1977 <sasa.juric@gmail.com>
2013-04-17 10:07:14 +0200

    expm

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(
+  name: "fun_chain", description: "Function chaining in Erlang",
+  version: "0.1", keywords: ["erlang"], 
+  maintainers: [[name: "Sasa Juric", email: "sasa.juric@gmail.com"]],
+  repositories: [[github: "sasa1977/fun_chain"]]
+)
